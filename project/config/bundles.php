1fcb7ce7379fd2591ac6750e928ba0aa373bd05a
Piotr Brzezina <piotr.brzezina@thesoftwarehouse.pl>
2020-02-25 21:39:39 +0100

    php 7.4.3

--- project/config/bundles.php
+++ project/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
